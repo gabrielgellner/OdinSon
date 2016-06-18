c29882cd726a1ef364057ea76a463e808594cf09
gabrielgellner <gabrielgellner@gmail.com>
2016-06-18 14:17:19 -0700

    Factor out the structure of the module

--- src/OdinSon.jl
+++ src/OdinSon.jl
@@ -3,6 +3,10 @@ module OdinSon
 # Seaborn like exports TODO: make these names more julian
 export set_palette, color_cycle
 
+# core types
+include("coretypes.jl")
+export Point
+
 # Colors
 import ColorTypes: HSV, RGB
 include("color_data/crayon.jl")
@@ -13,9 +17,19 @@ export PyObject # my overload so that RGB types are turned to python tuples
 export crayons, xkcd_rgb, svg_rgb
 export @NC_str, @HC_str, HSV, RGB
 
+# Styling
+include("style.jl")
+export Style
+
+# Containers
+include("containers.jl")
+export Canvas
+
 # Basic SVG/Mathematica like language
 include("shapes.jl")
-export Point
-export Canvas, Style, PolyLine, Polygon, Circle, render
+export PolyLine, Polygon, Circle
+
+# basic method for all graphic types
+export render
 
 end # module
--- src/shapes.jl
+++ src/shapes.jl
@@ -1,93 +1,9 @@
 #using PyPlot
 import Colors: RGB
 using PyCall
-import PyPlot: subplots
 @pyimport matplotlib.patches as mpatches
 @pyimport matplotlib.collections as mcol
 @pyimport matplotlib.lines as mlines
-
-type Style
-    stroke::Union{RGB, Void}
-    stroke_width::Float64
-    fill::Union{RGB, Void}
-    fill_opacity::Float64 # should check for being between 0-1
-    function Style(;stroke=NC"black", stroke_width=1.0, fill=NC"white", fill_opacity=1.0)
-        new(stroke, stroke_width, fill, fill_opacity)
-    end
-end
-
-# keywords for matplotlib patches are uniform
-function style2kw(s::Style)
-    Dict(:edgecolor => s.stroke, :linewidth => s.stroke_width, :facecolor => s.fill, :alpha => s.fill_opacity)
-end
-
-# keyworks for Line2D are different than patches
-function style2kw_l(s::Style)
-    Dict(:color => s.stroke, :linewidth => s.stroke_width)
-end
-
-# make the base shape types
-abstract Grob
-
-type Canvas
-    grobs::Array{Grob}
-    style::Style
-    function Canvas(grobs; style=Style(fill=NC"white"))
-        new(grobs, style)
-    end
-end
-
-function render(go::Canvas)
-    # using collections removes all styling, style must be passed into the PatchCollection
-    # patches = []
-    # for g in go.grobs
-    #     push!(patches, render(g))
-    # end
-    #pcol = mcol.PatchCollection(patches)
-    (fig, ax) = subplots()
-    # apply style to axes
-    ax[:patch][:set_facecolor](go.style.fill)
-    for g in go.grobs
-        p = render(g)
-        # assume that we have either lines or patches
-        if pyisinstance(p, mlines.Line2D)
-            ax[:add_line](p)
-        else
-            ax[:add_patch](p)
-        end
-    end
-    ax[:relim]() # resize the axes limits
-    ax[:autoscale_view]() # make sure everyting is visable
-    ax[:set_aspect]("equal") # TODO: make this an option
-    return fig
-end
-
-type Point
-    x::Float64
-    y::Float64
-end
-Base.zero(::Point) = Point(0.0, 0.0)
-
-function Base.convert(::Type{Point}, t::Union{Tuple, AbstractArray})
-    if length(t) != 2
-        throw(ArgumentError("Only length 2 objects can be converted to Point"))
-    end
-    Point(t[1], t[2])
-end
-PyCall.PyObject(t::Point) = PyObject((t.x, t.y))
-
-function Base.convert{T <: Real}(::Type{Array{Point}}, a::Array{T, 2})
-    (na, ma) = size(a)
-    #TODO: need to think about a 2x2 array, since it is ambigous, currently I assume
-    # column major
-    if na == 2
-        squeeze(mapslices(x->Point(x), a, 1), 1)
-    elseif ma == 2
-        squeeze(mapslices(x->Point(x), a, 2), 2)
-    else
-        throw(DimensionError())
-    end
-end
 
 type Circle <: Grob
     c::Point
