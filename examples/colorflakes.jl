7e4989e3f66184c010b0d311edddf27270215569
gabrielgellner <gabrielgellner@gmail.com>
2016-06-18 09:58:37 -0700

    Updated colorflake example

--- examples/colorflakes.jl
+++ examples/colorflakes.jl
@@ -20,45 +20,16 @@
 #      PlotPoints -> 200, PlotRange -> All, Background -> clr1]]
 #    , {n, 32}], 4], ImageSize -> 600]
 # ```
-# Can I recreate the above code? I am not sure exactly what is needed for ParametricPlot
-# So parameteric plot seems to generate lists of points that depend on a parameter
 using Distributions
 using OdinSon
 using PyPlot
-#
-# So for example the example ParametricPlot[{Sin[u], Sin[2u]}, {u, 0, 2pi}], generates
-# a series of {x, y} pairs given the relationship described, it shuld be the same as
-# ListLinePlot[Table[{Sin[u], Sin[2u]}, {u, 0, 2pi, inc}]]
-# the benefits just being that the sampling is done adaptivly
-u = linspace(0, 2π, 100)
-plot(sin(u), sin(2u))
-# good that is just what we wanted
 
-# Now the next question is how are nested pairs treated that is if we have
-# {{x1, y1}, {x2, y2}, {x3, y3}} instead of just {x, y}, what is done. My guess is that
-# you will get the number of pairs as individual plots. That is you could do this as three
-# plots ParametricPlot[{x1, y1}], ParametricPlot[{x2, y2}], ParametricPlot[{x3, y3}]
-#
-# the example
-# ParametricPlot[
-#   {{2 Cos[t], 2 Sin[t]}, {2 Cos[t], Sin[t]}, {Cos[t], 2 Sin[t]}, {Cos[t], Sin[t]}},
-#   {t, 0, 2 Pi},
-#   PlotLegends -> "Expressions"]
-t = linspace(0, 2π, 100)
-plot(2*cos(t), 2*sin(t))
-plot(2*cos(t), sin(t))
-plot(cos(t), 2*sin(t))
-plot(cos(t), sin(t))
-# perfect
-
-RGB(rand(3)...)
-
-# So we are ready
 function colorflake()
     clr1 = NC"black"
     clr2 = RGB(rand(3)...)
     clr3 = RGB(rand(3)...)
-    thick = rand(Uniform(2, 7)) # need to get this better, look into the units of mathematica + matplotlib
+    thick = rand(Uniform(2, 7)) # need to get this better, MM linewidth is based on size of figure
+    tm = 1 # this seems needless in the current code
 
     a = rand(Uniform(0, 1.5))
     b = rand(3:15)
@@ -68,9 +39,9 @@ function colorflake()
     x1(t) = sin(0.5*t) - a*sin(b*t).*cos(t) - 0.1*c*sin(10*b*t)
     y1(t) = cos(0.5*t) - a*sin(b*t).*sin(t) - 0.1*c*cos(10*b*t)
 
-    t = linspace(0, 4.0*π, 3000)
+    t = linspace(0, tm*4.0*π, 5000)
     plot(x1(t), y1(t), c=clr2, linewidth=thick)
-    plot(x1(t), y1(t), c=clr3, linewidth=0.1*thick)
+    plot(x1(t), y1(t), c=clr3, linewidth=0.2*thick)
     gca()[:set_aspect]("equal")
     gca()[:set_axis_bgcolor](clr1)
 end
